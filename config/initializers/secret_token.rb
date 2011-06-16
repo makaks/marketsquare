dea948b46f9e9638d0e23bd541514cb030dfa995
Makaks E <makak@yahoo.com>
2011-06-16 17:16:03 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Marketsquare::Application.config.secret_token = 'e342ca98905d7ae26b6f1624015ee5f3dbeab193958a4c135f4d1ba9fe8aa3199a5dabd18d4214c5a3765d60db92f9a6077be4b8593441faee3bfe62a76e9efd'
